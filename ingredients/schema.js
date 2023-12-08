3063e405fc77af634073748cf7a1f8794768a7e7
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-12-08 11:52:49 -0500

    Record the recipes that uses ingredients for the ingredientSchema. Easier for local database search.

--- ingredients/schema.js
+++ ingredients/schema.js
@@ -2,6 +2,14 @@ import mongoose from 'mongoose';
 
 const ingredientSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    usedBy: [
+        {
+            recipe: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'recipes'
+            },
+        }
+    ]
 }, { collection: 'ingredients' })
 
 export default ingredientSchema;
--- nutrients/schema.js
+++ nutrients/schema.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const nutrientSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true }
 }, { collection: 'nutrients' })
 
 export default nutrientSchema;
